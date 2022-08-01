74acfaed8277b9ea3249a1d02aa0b3c889f95e04
Glen112 <escobar.glen@gmail.com>
2022-08-01 17:55:52 -0300

    expreee-server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import express from 'express'
+
+const app = express()
+
+app.get('/status', (req:Request, res:Response, next:NextFunction)=> {
+  res.status(200).send({foo:'bar'})
+})
+
+app.listen(3000, ()=>{
+  console.log('executing appliction at 3000 port');
+  
+})
+
 console.log('typescript');
 
