f24a4eca01067cf3eec7790ce70ceb5893118b06
Glen112 <escobar.glen@gmail.com>
2022-08-01 20:30:33 -0300

    expreee-server-ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express()
 
 app.get('/status', (req:Request, res:Response, next:NextFunction)=> {
-  res.status(200).send({foo:'bar'})
+  res.status(200).send({foo:'excelente'})
 })
 
 app.listen(3000, ()=>{
