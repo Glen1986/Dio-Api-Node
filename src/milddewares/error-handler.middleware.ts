a74e345d6f6143edd726feb3b1db213ba6b3234c
Glen112 <escobar.glen@gmail.com>
2022-08-29 17:41:11 -0300

    basic-authentication

--- src/milddewares/error-handler.middleware.ts
+++ src/milddewares/error-handler.middleware.ts
@@ -1,10 +1,13 @@
 import {Request, Response, NextFunction} from 'express';
 import { StatusCodes } from "http-status-codes";
 import DatabaseError from '../models/errors/database.error.model';
+import ForbidemError from '../models/errors/forbidem.error.model';
 
 function errorHandler(e: any, req: Request, res: Response, next: NextFunction){
      if(e instanceof DatabaseError){
        res.sendStatus(StatusCodes.BAD_REQUEST)
+     } else if ( e instanceof ForbidemError ) {
+       res.sendStatus(StatusCodes.FORBIDDEN)
      } else{
       res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
   }
