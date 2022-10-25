a1e43397c8d578437a0d45a059bb83c00c8d1d12
Glen112 <escobar.glen@gmail.com>
2022-10-25 15:25:21 -0300

    bearer-token-commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import usersRoute from "./routes/users.route";
 import statusRoute from "./routes/status.route"
 import errorHandler from "./middlewares/error-Handler.middleware";
 import authorizationRoute from "./routes/authorization.route";
-import basicAuthenticationMiddleware from "./middlewares/basic-authentication.middleware";
+import bearerAuthenticationMiddleware from "./middlewares/bearer-authentication.middleware";
 
 const app = express();
 
@@ -12,15 +12,12 @@ app.use(express.json());
 app.use(urlencoded({extended: true}));
 
 //rutas de nuestra App
-app.use(usersRoute);
 app.use(statusRoute)
+app.use(bearerAuthenticationMiddleware, usersRoute);
 app.use(authorizationRoute)
 
 //error handler
 app.use(errorHandler)
-
-
-
 
 app.listen(3000, () => {
   console.log("3000 port aplication executing");
